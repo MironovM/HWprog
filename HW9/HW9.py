38ca51f1ced526ed163e38dc1a12b0c18d8fbccb
MironovM <31654797+MironovM@users.noreply.github.com>
2018-03-25 21:36:25 +0300

    Add files via upload

---
+++ HW9/HW9.py
@@ -0,0 +1,11 @@
+#v.2
+import re
+
+with open('Pol_Khofman_-_Levaya_Ruka_Boga.txt', encoding="utf-8") as f:
+    text = f.read()
+    results_part_1 = re.findall(' нашл[^ю]\w+', text)
+    results_part_2 = re.findall(' нашел\w+', text)
+    results_part_3 = re.findall(' найд\w+', text)
+    results_part_4 = re.findall(' нашед\w+', text)
+    all_results = results_part_1 + results_part_2 + results_part_3 + results_part_4
+    print(all_results)
