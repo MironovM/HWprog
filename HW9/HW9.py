dbea7c0672ab477b398c37d0b4ae6b7981878ec4
MironovM <31654797+MironovM@users.noreply.github.com>
2018-03-25 21:39:40 +0300

    Update HW9.py

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -3,7 +3,7 @@ import re
 
 with open('Pol_Khofman_-_Levaya_Ruka_Boga.txt', encoding="utf-8") as f:
     text = f.read()
-    results_part_1 = re.findall(' нашл[^ю]\w+', text)
+    results_part_1 = re.findall(' нашл[аои]\w+', text)
     results_part_2 = re.findall(' нашел\w+', text)
     results_part_3 = re.findall(' найд\w+', text)
     results_part_4 = re.findall(' нашед\w+', text)
