1d4feee15760a71b95aefa78ed73967d9af53477
MironovM <31654797+MironovM@users.noreply.github.com>
2018-03-26 22:03:11 +0300

    Update HW9.py

--- HW9/HW9.py
+++ HW9/HW9.py
@@ -1,7 +1,7 @@
 #v.2
 import re
 
-def function1():
+def function_1():
     with open('Pol_Khofman_-_Levaya_Ruka_Boga.txt', encoding="utf-8") as f:
         text = f.read()
         results_part_1 = re.findall(' нашл[аио]\w+', text)
@@ -10,7 +10,7 @@ def function1():
         results_part_4 = re.findall(' нашед\w+', text)
         all_results = results_part_1 + results_part_2 + results_part_3 + results_part_4
     return all_results
-def function2(all_results):
+def function_2(all_results):
     dic = {}
     for word in all_results:
         if word in dic:
@@ -18,8 +18,8 @@ def function2(all_results):
         else:
             dic[word] = 1
     return dic
-def function3():
-    sth = function1()
-    print(function2(sth))
+def function_3():
+    sth = function_1()
+    print(function_2(sth))
 
-function3()
+function_3()
